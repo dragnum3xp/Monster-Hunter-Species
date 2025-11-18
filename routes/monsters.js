2bda191322ec377d4b6af661b3a570779a36a815
Dragnum <dragnumexp@gmail.com>
2025-11-18 11:51:25 -0300

    fixing variable

--- routes/monsters.js
+++ routes/monsters.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 const monstersController = require("../controllers/monsters");
+const validation = require('../middleware/validate');
 
 router.get("/", monstersController.getAll);
 
